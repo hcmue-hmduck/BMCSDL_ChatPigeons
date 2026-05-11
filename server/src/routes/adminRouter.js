cb796938549c6a9f20e46c6515f86a3ca6db1c0a
vuthetam <tamvuthe05@gmail.com>
2026-05-11 13:05:39 +0700

    Fea(auth): Implement role-based authorization

--- server/src/routes/adminRouter.js
+++ server/src/routes/adminRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { authorize } = require('../middlewares/authMiddleware.js');
 
 const usersRouter = require('./usersRouter');
 
+router.use(authorize(['admin']));
 router.use('/users', usersRouter);
 
 module.exports = router;
